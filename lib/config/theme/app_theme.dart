52d00c4106136c2263465c01f3f983dc1388dfc3
Edwin Quebrada <edanwitunry@gmail.com>
2023-06-04 14:00:36 -0500

    clase theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.pink
+];
+
+class AppTheme {
+  final int selectodColor;
+
+  AppTheme({
+    this.selectodColor = 0,
+  })  : assert(selectodColor >= 0, 'El color seleccionado debe ser mayor a 0'),
+        assert(selectodColor < colorList.length,
+            'El color seleccionado debe ser menos a ${colorList.length-1}');
+
+  ThemeData getTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectodColor]);
+}
