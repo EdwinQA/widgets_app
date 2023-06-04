071fb4e86b589276301ae2512eaf0fc2570c6631
Edwin Quebrada <edanwitunry@gmail.com>
2023-06-04 16:24:05 -0500

    go_routerConfigurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -38,7 +39,9 @@ class _HomeView extends StatelessWidget {
             //    builder: (context) => const ButtonScreen(),
             //  ),
             //);
-            Navigator.pushNamed(context, menuItem.link);
+            //Navigator.pushNamed(context, menuItem.link);
+
+            context.push(menuItem.link);
           },
         );
       },
