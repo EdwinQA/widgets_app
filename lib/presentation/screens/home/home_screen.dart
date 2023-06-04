b6d553b4da3aefa5f1ec48980668a64c8b29a3d3
Edwin Quebrada <edanwitunry@gmail.com>
2023-06-04 15:02:27 -0500

    home_screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,8 +8,32 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter + Material 3'),
+        title: const Text('Flutter + Material 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return ListTile(
+          title: Text(menuItem.title),
+          subtitle: Text(menuItem.subTitle),
+          leading: Icon(menuItem.icon, color: colors.primary),
+          trailing:
+              Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+        );
+      },
     );
   }
 }
